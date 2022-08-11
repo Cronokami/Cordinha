e1156636e4cfe99312f5a63ad982b9af411f038e
César Neves Mosca <Cronokami@hotmail.com>
2022-08-11 00:47:19 -0300

    Sistema de level procedural e parede lateral infinita
    Talvez valha a pena mudar a parede depois, e o sistema procedural tem um limite pequeno por enquanto e não tá por pool

---
+++ WormsRoper/Assets/Scripts/PlatformSpawner.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformSpawner : MonoBehaviour
+{
+	public GameObject PrefabPlatform;
+	public float startPlatforms;
+	public float horizontalDistance;
+	public float verticalDistance;
+
+	private float platformPositionY;
+	
+
+	private void Start()
+	{
+		for (int i = 0; i < startPlatforms; i++)
+		{
+			platformPositionY = i*verticalDistance;
+			CreatePlatform();
+		}
+		
+	}
+
+	private void Update()
+	{
+		
+	}
+
+	private void CreatePlatform()
+	{
+		int newPlatformType = Random.Range(-1, 3);
+		Debug.Log(newPlatformType);
+		float platformPositionX;
+
+		if (newPlatformType < 2)
+		{
+			platformPositionX = newPlatformType * horizontalDistance;
+		}
+		else
+		{
+			platformPositionX = 1 * horizontalDistance;
+			Instantiate(PrefabPlatform, new Vector3(-platformPositionX, platformPositionY, 0), Quaternion.identity, transform);
+		}
+
+		Instantiate(PrefabPlatform, new Vector3(platformPositionX, platformPositionY, 0), Quaternion.identity, transform);
+	}
+
+}
---
+++ WormsRoper/Assets/Scripts/WallFollow.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallFollow : MonoBehaviour
+{
+	public GameObject Player;
+
+    private void FixedUpdate()
+    {
+		transform.position = Vector3.Lerp(transform.position, new Vector3(transform.position.x, Player.transform.position.y, transform.position.z), 0.5f);
+    }
+}
